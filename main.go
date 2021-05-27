c7b45d21ff60d7776bcf7de563aa068fd55a265c
Aditya Meharia <adityameharia14@gmail.com>
2021-05-27 11:53:59 +0530

    defined config

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
-	"github.com/adityameharia/ravel/datastore"
-	"github.com/joho/godotenv"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/adityameharia/ravel/datastore"
+	"github.com/joho/godotenv"
 )
 
+var dir string
+var gRPCaddr string
+var id string
+var join string
+var raftAddr string
+
+type Config struct {
+	dir      string
+	gRPCaddr string
+	id       string
+	join     string
+	raftAddr string
+}
+
+func init() {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	flag.StringVar(&dir, "raftDir", dirname, "raft data directory")
+	flag.StringVar(&gRPCaddr, "addr", ":5000", "server listen address")
+	flag.StringVar(&id, "id", "", "replica id")
+	flag.StringVar(&join, "join", "", "join to already exist cluster")
+	flag.StringVar(&raftAddr, "raftAddr", "", "Set Raft internal communication address")
+
+}
+
 func main() {
+	flag.Parse()
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -15,5 +47,15 @@ func main() {
 
 	badgerPath := os.Getenv("BADGER_PATH")
 	err = datastore.Init(badgerPath)
-	datastore.Close()
+	defer datastore.Close()
+
+	c := &Config{
+		dir:      dir,
+		gRPCaddr: gRPCaddr,
+		id:       id,
+		join:     join,
+		raftAddr: raftAddr,
+	}
+	fmt.Println(c)
+
 }
