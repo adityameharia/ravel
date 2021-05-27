350c537aa3fe8e2d911dd3a86d66b8b2c55c4d79
Junaid Rahim <junaidrahim8d@gmail.com>
2021-05-27 12:22:44 +0530

    syncwrite true

--- datastore/init.go
+++ datastore/init.go
@@ -10,8 +10,11 @@ var db *badger.DB
 // Init initialises BadgerDB with the path provided.
 func Init(path string) error {
 	var err error
+
 	options := badger.DefaultOptions(path)
 	options.Logger = nil
+	options.SyncWrites = true
+
 	db, err = badger.Open(options)
 	if err != nil {
 		return err
