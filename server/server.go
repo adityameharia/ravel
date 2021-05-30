9623153ad1cce0dc2fa5aceaa79af89872868af1
Junaid Rahim <junaidrahim8d@gmail.com>
2021-05-30 16:07:29 +0530

    proto changes

--- server/server.go
+++ server/server.go
@@ -8,19 +8,11 @@ import (
 
 type Server struct{}
 
-func (s *Server) AcceptJoin(ctx context.Context, req *RavelClusterPB.Join) (*RavelClusterPB.Response, error) {
+func (s *Server) Join(ctx context.Context, req *RavelClusterPB.Node) (*RavelClusterPB.Response, error) {
 	return nil, nil
 }
 
-func (s *Server) RequestJoin(ctx context.Context, req *RavelClusterPB.Join) (*RavelClusterPB.Response, error) {
-	return nil, nil
-}
-
-func (s *Server) AcceptLeave(ctx context.Context, req *RavelClusterPB.Node) (*RavelClusterPB.Response, error) {
-	return nil, nil
-}
-
-func (s *Server) RequestLeave(ctx context.Context, req *RavelClusterPB.Node) (*RavelClusterPB.Response, error) {
+func (s *Server) Leave(ctx context.Context, req *RavelClusterPB.Node) (*RavelClusterPB.Response, error) {
 	return nil, nil
 }
 
