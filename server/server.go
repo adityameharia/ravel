0559526351fa41de4e9d89c3d249f82830fed0d6
Junaid Rahim <junaidrahim8d@gmail.com>
2021-05-31 00:53:51 +0530

    implemented Run in server.go

--- server/server.go
+++ server/server.go
@@ -35,5 +35,26 @@ func (s *Server) Leave(ctx context.Context, req *RavelClusterPB.Node) (*RavelClu
 }
 
 func (s *Server) Run(ctx context.Context, req *RavelClusterPB.Command) (*RavelClusterPB.Response, error) {
-	return nil, nil
+	switch req.Operation {
+	case "get":
+		val, err := s.Node.Get(req.Key)
+		if err != nil {
+			return nil, err
+		}
+		return &RavelClusterPB.Response{Data: val}, nil
+	case "set":
+		err := s.Node.Set(req.Key, req.Value)
+		if err != nil {
+			return nil, err
+		}
+		return &RavelClusterPB.Response{Data: "set successful"}, nil
+	case "delete":
+		err := s.Node.Delete(req.Key)
+		if err != nil {
+			return nil, err
+		}
+		return &RavelClusterPB.Response{Data: "delete successful"}, nil
+	default:
+		return nil, errors.New("invalid operation")
+	}
 }
